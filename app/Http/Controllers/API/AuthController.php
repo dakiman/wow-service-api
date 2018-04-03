9178b5e72808fb52121febcbd3563c2c30f85561
dakiman <dvancov@hotmail.com>
2018-04-03 15:50:50 +0200

    Added user route, code prettier

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -8,58 +8,56 @@ use App\User;
 use DB;
 use Hash;
 use Illuminate\Support\Facades\Validator;
-
 class AuthController extends Controller
 {
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:50|unique:users',
-            'name' => 'required|string|max:30|min:2',
-            'password' => 'required|string|min:8'
-        ]);
+  public function register(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'email' => 'required|string|email|max:50|unique:users',
+      'name' => 'required|string|max:30|min:2',
+      'password' => 'required|string|min:8'
+    ]);
 
-        if($validator->fails()){
-            return response()->json(['errors'=>$validator->getMessageBag(),'status' => 400]);
-        } else {
-            User::create([
-                'name' => request('name'),
-                'email' => request('email'),
-                'password' => bcrypt(request('password'))
-            ]);
-            return response()->json(['status' => 201]);
-        }
+    if ($validator->fails()) {
+      return response()->json(['errors' => $validator->getMessageBag(), 'status' => 400]);
+    } else {
+      User::create([
+        'name' => request('name'),
+        'email' => request('email'),
+        'password' => bcrypt(request('password'))
+      ]);
+      return response()->json(['status' => 201]);
+    }
+  }
+
+  public function login()
+  {
+    $result = auth()->attempt(request(['email', 'password']));
+    if (!$result) {
+      return response()->json([
+        'errors' => [
+          'auth' => [
+            'Wrong email or password.'
+          ]
+        ],
+      ], 422);
     }
 
-    public function login()
-    {
-        $result = auth()->attempt(request(['email', 'password']));
-        if(!$result)
-        {
-            return response()->json([
-                'errors' => [
-                    'auth'=> [
-                        'Wrong email or password.'
-                    ]
-                ],
-            ], 422);
-        }
+    $user = User::whereEmail(request('email'))->first();
 
-        $user = User::whereEmail(request('email'))->first();
-        
         // Send an internal API request to get an access token
-        $data = [
-            'grant_type' => 'password',
-            'client_id' => '2',
-            'client_secret' => env('OAUTH_PERSONAL_KEY'),
-            'username' => request('email'),
-            'password' => request('password'),
-        ];
+    $data = [
+      'grant_type' => 'password',
+      'client_id' => '2',
+      'client_secret' => env('OAUTH_PERSONAL_KEY'),
+      'username' => request('email'),
+      'password' => request('password'),
+    ];
 
         /*
             -------------------------------
             CHECK CODE BLOCK FOR USEFULLNESS
-        */
+     */
 
         // $accessToken = $user->token();
 
@@ -72,40 +70,45 @@ class AuthController extends Controller
 
         /*
             -------------------------------
-        */
+     */
 
-        $request = Request::create('/oauth/token', 'POST', $data);
+    $request = Request::create('/oauth/token', 'POST', $data);
 
-        $response = app()->handle($request);
+    $response = app()->handle($request);
 
         // Check if the request was successful
-        if ($response->getStatusCode() != 200) {
-            return response()->json([
-                'message' => 'Request cannot be handled',
-                'status' => $response->getStatusCode()    
-            ], 422);
-        }
-
-        $data = json_decode($response->getContent());
-
-        return response()->json([
-            'token' => $data->access_token,
-            'user' => $user,
-        ], 200);
+    if ($response->getStatusCode() != 200) {
+      return response()->json([
+        'message' => 'Request cannot be handled',
+        'status' => $response->getStatusCode()
+      ], 422);
     }
 
-    public function logout()
-    {
-        $accessToken = auth()->user()->token();
+    $data = json_decode($response->getContent());
 
-        $refreshToken = DB::table('oauth_refresh_tokens')
-            ->where('access_token_id', $accessToken->id)
-            ->update([
-                'revoked' => true
-            ]);
+    return response()->json([
+      'token' => $data->access_token,
+      'user' => $user,
+    ], 200);
+  }
 
-        $accessToken->revoke();
+  public function logout()
+  {
+    $accessToken = auth()->user()->token();
 
-        return response()->json(['status' => 200]);
-    }
+    $refreshToken = DB::table('oauth_refresh_tokens')
+      ->where('access_token_id', $accessToken->id)
+      ->update([
+        'revoked' => true
+      ]);
+
+    $accessToken->revoke();
+
+    return response()->json(['status' => 200]);
+  }
+
+  public function getUser()
+  {
+      return auth()->user();
+  }
 }
