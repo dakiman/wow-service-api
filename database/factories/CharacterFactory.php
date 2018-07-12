6701ef4d07bab9a7ccba698ecc6cb7b6c72d7bce
Dragan Vanchov <Dragan.Vanchov@endava.com>
2018-07-12 17:33:46 +0200

    Fixed character factory to include all values returned by api

--- database/factories/CharacterFactory.php
+++ database/factories/CharacterFactory.php
@@ -6,5 +6,14 @@ $factory->define(App\Character::class, function (Faker $faker) {
     return [
 		'name' => str_random(10),
 		'realm' => str_random(10),
+		'class' => random_int(1, 12),
+		'thumbnail' => str_random(12),
+		'battlegroup' => str_random(12),
+		'faction' => random_int(0, 1),
+		'gender' => random_int(0, 1),
+		'race' => random_int(1, 22),
+		'level' => random_int(0, 110),
+		'totalHonorableKills' => random_int(0, 100000),
+		'achievementPoints' => random_int(0, 15000)
     ];
 });
